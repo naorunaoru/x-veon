c514de46cffec214329a18ab9d2dfb3b1c4b26e2
Roman Kuraev <naoru.desu@gmail.com>
2026-02-09 19:10:58 +0000

    fix localhost vite

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 
-export default defineConfig({
-  base: '/x-veon/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/x-veon/' : '/',
   plugins: [wasm()],
   optimizeDeps: {
     exclude: ['onnxruntime-web'],
@@ -17,4 +17,4 @@ export default defineConfig({
   build: {
     target: 'esnext',
   },
-});
+}));
